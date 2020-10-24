6232fa1a51fab5558bfc397c69ab75e25d698068
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-10-25 05:28:01 +0600

    doing nothing but structuring the application

---
+++ commands/key.js
@@ -0,0 +1,14 @@
+const key = {
+    set(){
+        console.log("Hello from set method");
+    },
+    show(){
+        console.log("Hello from show method");
+    },
+    remove(){
+        console.log("Hello from remove method");
+    }
+}
+
+
+module.exports = key;
