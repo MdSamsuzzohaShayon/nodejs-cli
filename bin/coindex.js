7fd07a020b99c60214b9f738255f61af740db1f4
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-10-25 05:10:46 +0600

    Top lavel commands

--- bin/coindex.js
+++ bin/coindex.js
@@ -13,7 +13,10 @@ const pkg  = require('../package.json');
 // SETTING VERSION AND PARSE AS SO IT WILL ACCESS ALL ARGUMENTS
 // program.version('1.0.0').parse(process.argv);
 // SETTING VERSION FROM PACKAGE.JSON
-program.version(pkg.version).parse(process.argv);
+program
+    .version(pkg.version)
+    .command('key', "Manage API key -- https://nomics.com") // TOP LAVEL COMMANDS
+    .parse(process.argv);
 
 
 console.log("Hello from coindex.js");
