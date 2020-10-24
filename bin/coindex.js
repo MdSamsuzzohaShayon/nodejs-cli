a7a048da0d4a9ea4edde9e8bf3f040f734ec3436
Md Samsuzzoha Shayon <mdshayon0@gmail.com>
2020-10-25 05:08:31 +0600

    help  and version commands

--- bin/coindex.js
+++ bin/coindex.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
+// https://docs.npmjs.com/files/package.json#bin
+// THIS LINE WHOULD BE AT THE VERY TOP 
+
+
+
 
 
 const program = require("commander");
 
+const pkg  = require('../package.json');
+
 // SETTING VERSION AND PARSE AS SO IT WILL ACCESS ALL ARGUMENTS
-program.version('1.0.0').parse(process.argv);
+// program.version('1.0.0').parse(process.argv);
+// SETTING VERSION FROM PACKAGE.JSON
+program.version(pkg.version).parse(process.argv);
 
 
 console.log("Hello from coindex.js");
